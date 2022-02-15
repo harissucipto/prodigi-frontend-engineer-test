0093ba6dabc3a1002cbd8e4d32e30e6932efecae
Haris Sucipto <harisssucipto@gmail.com>
2022-02-15 18:41:14 +0800

    add redirect to Main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,24 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 const Home: NextPage = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/asuransi/jiwa");
+  }, [router]);
+
   return (
     <div>
       <Head>
         <title>Prodigi Frontend Engineer Test</title>
       </Head>
       <div>
-        <p className="font-bold">Hello World</p>
+        <p className="font-bold text-center animate-bounce p-20">
+          Redirecting to Route App...
+        </p>
       </div>
     </div>
   );
